673e658fe651ae1ed97aebee028c8cb0571738fd
PavloGV <pavlogv@gmail.com>
2018-03-11 19:15:27 -0700

    re-added sudo
    re-added sudo

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@
 # How to use:
 # 	- 	Initially running 'run.sh' will execute both the throughput and latency benchmarks
 #		with all threads all input sizes, etc... This can take a long time.
-sh -c 'echo -1 >/proc/sys/kernel/perf_event_paranoid'
-sh -c 'echo 0 >/proc/sys/kernel/nmi_watchdog'
+sudo sh -c 'echo -1 >/proc/sys/kernel/perf_event_paranoid'
+sudo sh -c 'echo 0 >/proc/sys/kernel/nmi_watchdog'
 echo '================ in run.sh =================='
 declare -a divType=(256 64 16 4 1) 		# **** Corresponds inputSizes order *****
 declare -a measName=(IPC BranchMPKI LoneMPKI LLC)
@@ -26,9 +26,9 @@ do
 		do 
 			echo '-------------------------------------------------------------------'
 			echo 'Thread number: '$t', inputSize: '${inputSizes[index]}', divType: '$d', measure: '$m
-			python cmpe202w2018/project/filterpy/filterpy/kalman/tests/noisy_sine.py -l ${inputSizes[index]} -t $t
-			perf stat -I $instCountNum -e cpu-cycles,instructions,branch-instructions,branch-misses,L1-dcache-load,LLC-loads -x, -o intermediate.csv python cmpe202w2018/project/filterpy/filterpy/kalman/tests/read_signal.py; python cmpe202w2018/project/filterpy/filterpy/kalman/tests/../../../../../pmu-tools/interval-normalize.py intermediate.csv > normOut.csv
-			python cmpe202w2018/project/filterpy/filterpy/kalman/tests/plotMyData.py -f normOut.csv -d $d -m $m -t $t
+			sudo python cmpe202w2018/project/filterpy/filterpy/kalman/tests/noisy_sine.py -l ${inputSizes[index]} -t $t
+			sudo perf stat -I $instCountNum -e cpu-cycles,instructions,branch-instructions,branch-misses,L1-dcache-load,LLC-loads -x, -o intermediate.csv python cmpe202w2018/project/filterpy/filterpy/kalman/tests/read_signal.py; python cmpe202w2018/project/filterpy/filterpy/kalman/tests/../../../../../pmu-tools/interval-normalize.py intermediate.csv > normOut.csv
+			sudo python cmpe202w2018/project/filterpy/filterpy/kalman/tests/plotMyData.py -f normOut.csv -d $d -m $m -t $t
 			index=$(( $index + 1 ))	
 		done
 		index=0
@@ -52,12 +52,12 @@ echo 'Beginning Latnecy benchmark measurements '
 python noisy_sine.py -l 200 -t 1
 while [ $index -lt $instCountNum ]
 do
-  perf stat -I 100 -e cycles -x, -o intermediate.csv python cmpe202w2018/project/filterpy/filterpy/kalman/tests/read_signal.py; python cmpe202w2018/project/filterpy/filterpy/kalman/tests/../../../../../pmu-tools/interval-normalize.py intermediate.csv > normOut.csv
-	python cmpe202w2018/project/filterpy/filterpy/kalman/tests/get_run_time.py -f normOut.csv -n $index
+	sudo perf stat -I 100 -e cycles -x, -o intermediate.csv python cmpe202w2018/project/filterpy/filterpy/kalman/tests/read_signal.py; python cmpe202w2018/project/filterpy/filterpy/kalman/tests/../../../../../pmu-tools/interval-normalize.py intermediate.csv > normOut.csv
+	sudo python cmpe202w2018/project/filterpy/filterpy/kalman/tests/get_run_time.py -f normOut.csv -n $index
 	echo 'run: '$index
 	index=$(( $index + 1 ))
 done
-python cmpe202w2018/project/filterpy/filterpy/kalman/tests/plotLatency.py
+sudo python cmpe202w2018/project/filterpy/filterpy/kalman/tests/plotLatency.py
 echo 'Latency benchmark measurements completed'
 
 ### last things to do
